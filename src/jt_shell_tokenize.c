0d8a21ee4443d2e2c99058b6205eb6db19be05a2
Jakub Tymejczyk <jakub@tymejczyk.pl>
2020-04-18 21:03:10 +0200

    Rozbicie na tokeny na podstawie maszyny stanow

--- src/jt_shell_tokenize.c
+++ src/jt_shell_tokenize.c
@@ -7,30 +7,52 @@
 #include <string.h>
 #include <unistd.h>
 
-
 #include "stdio.h"
 
-static bool
-jt_shell_tokenize_arg_is_empty(const char *arg)
+typedef enum {
+    CHAR = 1,
+    ESCAPE,
+    QUOTE,
+    SPACE,
+    PIPE,
+    SREDNIK, //TODO
+    REDIRECT_IN,
+    REDIRECT_OUT,
+    EOL,
+} jt_shell_tokenize_type_t;
+
+typedef struct {
+    bool in_quote;
+    bool redirect_active;
+    bool arg_active;
+} jt_shell_tokenize_ctx_t;
+
+static jt_shell_tokenize_ctx_t jt_shell_tokenize_ctx;
+
+static jt_shell_tokenize_type_t
+jt_shell_tokenize_get_token(char ch)
 {
-
-    while (*arg) {
-        bool match = false;
-
-        for (const char *c = JT_SHELL_TOKEN_DELIM; '\0' != *c; c++) {
-            if (*arg == *c) {
-                match = true;
-                break;
-            }
-        }
-        if (!match) {
-            return false;
-        }
-
-        arg++;
-    }
+    jt_shell_tokenize_type_t token = CHAR;
+    const char *delims = JT_SHELL_TOKEN_DELIM;
     
-    return true;
+    if (ch == '\0') {
+        return EOL;
+    } else if (ch == ';' && !jt_shell_tokenize_ctx.in_quote) {
+        return SREDNIK;
+    } else if (ch == '|' && !jt_shell_tokenize_ctx.in_quote) {
+        return PIPE;
+    } else if (ch == '<' && !jt_shell_tokenize_ctx.in_quote) {
+        return REDIRECT_IN;
+    } else if (ch == '>' && !jt_shell_tokenize_ctx.in_quote) {
+        return REDIRECT_OUT;
+    }
+
+    for (const char *c = delims; '\0' != *c; c++) {
+        if (*c == ch && !jt_shell_tokenize_ctx.in_quote) {
+            return SPACE;
+        }
+    }
+    return token;
 }
 
 void
@@ -38,33 +60,30 @@ jt_shell_tokenice_arg_end(char *arg,
                           jt_shell_cmds_t *group,
                           jt_shell_cmd_t *cur_cmd,
                           size_t group_cnt,
-                          size_t *args_sz,
+                          size_t *args_cnt,
                           bool *in, bool *out)
 {
-    if (!jt_shell_tokenize_arg_is_empty(arg)) {
-        if (*in) {
-            group[group_cnt - 1].redirect_in = arg;
-            *in = false;
-        } else if (*out) {
-            group[group_cnt - 1].redirect_out = arg;
-            *out = false;
-        } else {
-            cur_cmd->tokens[(*args_sz)++] = arg;
-        }
+    if (*in) {
+        group[group_cnt - 1].redirect_in = arg;
+        *in = false;
+    } else if (*out) {
+        group[group_cnt - 1].redirect_out = arg;
+        *out = false;
+    } else {
+        cur_cmd->tokens[(*args_cnt)++] = arg;
     }
 }
 
 jt_shell_cmds_t *
 jt_shell_tokenize(char *line)
 {
-    const char *delims = JT_SHELL_TOKEN_DELIM;
 
     jt_shell_cmds_t *cmds = calloc(sizeof(jt_shell_cmds_t), 1);
     cmds->cmds = malloc(JT_SHELL_CMDS_COUNT * sizeof(jt_shell_cmd_t *));
 
     size_t group_cnt = 0;
     size_t args_cnt = 0;
-    size_t args_sz;
+    size_t args_sz = 0;
     char **args = NULL;
     char *arg_start = NULL;
 
@@ -76,6 +95,7 @@ jt_shell_tokenize(char *line)
 
     jt_shell_cmd_t *cur_cmd = NULL;
 
+    jt_shell_tokenize_type_t token;
     do {
         if (new_group) {
             new_cmd = true;
@@ -93,9 +113,9 @@ jt_shell_tokenize(char *line)
                 cur_cmd->next = NULL;
             }
             
-            args_cnt = JT_SHELL_ARGS_COUNT;
-            args_sz = 0;
-            args = calloc(args_cnt * sizeof(char *), 1);
+            args_cnt = 0;
+            args_sz = JT_SHELL_ARGS_COUNT;
+            args = calloc(args_sz * sizeof(char *), 1);
 
             if (NULL == args) {
                 jt_logger_log(JT_LOGGER_LEVEL_ERROR,
@@ -103,69 +123,69 @@ jt_shell_tokenize(char *line)
                             "Args split cannot alloc");
                 return NULL;
             }
-            
-            arg_start = line;
 
             cur_cmd->tokens = args;
             new_cmd = false;
             new_group = false;
         }
-
-        //for loop is strtok implementation
-        for (const char *c = delims; '\0' != *c; c++) {
-            if (*c == *line && *(line - 1) != '\0') {
-                *line = '\0';
+        char ch = *line;
+        token = jt_shell_tokenize_get_token(ch);
+        
+        switch (token) {
+        case EOL:
+            if (jt_shell_tokenize_ctx.arg_active) {
                 jt_shell_tokenice_arg_end(arg_start,
-                                          cmds,
-                                          cur_cmd,
-                                          group_cnt,
-                                          &args_sz,
-                                          &redirect_in, &redirect_out);
+                                            cmds,
+                                            cur_cmd,
+                                            group_cnt,
+                                            &args_cnt,
+                                            &redirect_in, &redirect_out);
+            }
+            memset(&jt_shell_tokenize_ctx, 0, sizeof(jt_shell_tokenize_ctx));
+            break;
+        case CHAR:
+            if (!jt_shell_tokenize_ctx.arg_active) {
+                arg_start = line;
+            }
+            jt_shell_tokenize_ctx.arg_active = true;
+            break;
+        case QUOTE:
+            break;
+        case SPACE:
+            *line = '\0';
+            if (!jt_shell_tokenize_ctx.arg_active) {
                 arg_start = line + 1;
-                if (args_cnt >= args_sz) {
-                    args_cnt += JT_SHELL_ARGS_COUNT;
-                    args = realloc(args, args_cnt * sizeof(char *));
-                    cur_cmd->tokens = args;
-                    if (NULL == args) {
-                        jt_logger_log(JT_LOGGER_LEVEL_ERROR,
-                                    "%s\n",
-                                    "Args split cannot realloc");
-                        return NULL;
-                    }
-                }
-                break;
-            } else if (*c == *line) {
-                *line = '\0';
-                arg_start++;
                 break;
             }
-        }
-
-        if (';' == *line) {
-            *line = '\0';
             jt_shell_tokenice_arg_end(arg_start,
-                                        cmds,
-                                        cur_cmd,
-                                        group_cnt,
-                                        &args_sz,
-                                        &redirect_in, &redirect_out);
-            arg_start = line + 1;
-            new_group = true;
-            jt_logger_log_array(JT_LOGGER_LEVEL_DEBUG,
-                                args);
-            jt_logger_log(JT_LOGGER_LEVEL_ERROR,
-                        "%s\n",
-                        "new group");
-        }
-
-        if ('|' == *line) {
+                                      cmds,
+                                      cur_cmd,
+                                      group_cnt,
+                                      &args_cnt,
+                                      &redirect_in, &redirect_out);
+            if (args_cnt >= args_sz) {
+                args_sz += JT_SHELL_ARGS_COUNT;
+                args = realloc(args, args_sz * sizeof(char *));
+                cur_cmd->tokens = args;
+                if (NULL == args) {
+                    jt_logger_log(JT_LOGGER_LEVEL_ERROR,
+                                "%s\n",
+                                "Args split cannot realloc");
+                    return NULL;
+                }
+            }
+            jt_shell_tokenize_ctx.arg_active = false;
+            break;
+        case PIPE:
             *line = '\0';
-            jt_shell_tokenice_arg_end(arg_start,
-                                        cmds,
-                                        cur_cmd,
-                                        group_cnt,
-                                        &args_sz,
-                                        &redirect_in, &redirect_out);
+            if (jt_shell_tokenize_ctx.arg_active) {
+                jt_shell_tokenice_arg_end(arg_start,
+                                            cmds,
+                                            cur_cmd,
+                                            group_cnt,
+                                            &args_cnt,
+                                            &redirect_in, &redirect_out);
+            }
             arg_start = line + 1;
             new_cmd = true;
             jt_logger_log_array(JT_LOGGER_LEVEL_DEBUG,
@@ -173,16 +193,36 @@ jt_shell_tokenize(char *line)
             jt_logger_log(JT_LOGGER_LEVEL_ERROR,
                         "%s\n",
                         "new cmd");
-        }
-
-        if ('<' == *line) {
+            break;
+        case SREDNIK:
             *line = '\0';
-            jt_shell_tokenice_arg_end(arg_start,
-                                        cmds,
-                                        cur_cmd,
-                                        group_cnt,
-                                        &args_sz,
-                                        &redirect_in, &redirect_out);
+            if (jt_shell_tokenize_ctx.arg_active) {
+                jt_shell_tokenice_arg_end(arg_start,
+                                            cmds,
+                                            cur_cmd,
+                                            group_cnt,
+                                            &args_cnt,
+                                            &redirect_in, &redirect_out);
+            }
+            arg_start = line + 1;
+            new_group = true;
+            jt_logger_log_array(JT_LOGGER_LEVEL_DEBUG,
+                                args);
+            jt_logger_log(JT_LOGGER_LEVEL_ERROR,
+                        "%s\n",
+                        "new group");
+            jt_shell_tokenize_ctx.arg_active = false;
+            break;
+        case REDIRECT_IN:
+            *line = '\0';
+            if (jt_shell_tokenize_ctx.arg_active) {
+                jt_shell_tokenice_arg_end(arg_start,
+                                            cmds,
+                                            cur_cmd,
+                                            group_cnt,
+                                            &args_cnt,
+                                            &redirect_in, &redirect_out);
+            }
             arg_start = line + 1;
             redirect_in = true;
             jt_logger_log_array(JT_LOGGER_LEVEL_DEBUG,
@@ -190,16 +230,17 @@ jt_shell_tokenize(char *line)
             jt_logger_log(JT_LOGGER_LEVEL_DEBUG,
                         "%s\n",
                         "new redirect_in");
-        }
-
-        if ('>' == *line) {
+            break;
+        case REDIRECT_OUT:
             *line = '\0';
-            jt_shell_tokenice_arg_end(arg_start,
-                                        cmds,
-                                        cur_cmd,
-                                        group_cnt,
-                                        &args_sz,
-                                        &redirect_in, &redirect_out);
+            if (jt_shell_tokenize_ctx.arg_active) {
+                jt_shell_tokenice_arg_end(arg_start,
+                                            cmds,
+                                            cur_cmd,
+                                            group_cnt,
+                                            &args_cnt,
+                                            &redirect_in, &redirect_out);
+            }
             arg_start = line + 1;
             redirect_out = true;
             jt_logger_log_array(JT_LOGGER_LEVEL_DEBUG,
@@ -207,16 +248,12 @@ jt_shell_tokenize(char *line)
             jt_logger_log(JT_LOGGER_LEVEL_DEBUG,
                         "%s\n",
                         "new redirect_out");
+            break;
+        case ESCAPE:
+            break;
         }
-
-    } while (*(++line));
-
-    jt_shell_tokenice_arg_end(arg_start,
-                                cmds,
-                                cur_cmd,
-                                group_cnt,
-                                &args_sz,
-                                &redirect_in, &redirect_out);
+        line++;
+    } while (token != EOL);
 
     cmds->group_cnt = group_cnt;
 
